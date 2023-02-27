a113d0c57982b5c8ed0e544635dd6bb40416934a
MarceliG <marcel9817@gmail.com>
2023-02-27 18:27:41 +0100

    test_2_v1

---
+++ test_2.py
@@ -0,0 +1,21 @@
+def test_2():
+    
+    a     =  []
+    
+    for i in range(10):
+        
+        
+        
+        a.append(i       
+                 
+                 
+                 )
+        
+
+if __name__ =="__main__":
+    
+    
+    test_2(
+        
+        
+    )
